3a96b8bcf07a70eafad2701b22cab72edb3bf440
Nehal Jaisalmeria <nehal.jaisalmeria@sapidblue.com>
2022-10-16 12:04:26 +0530

    Added posts and message

---
+++ components/message.js
@@ -0,0 +1,14 @@
+export default function Message({ children }) {
+  return (
+    <div className="bg-white p-8 border-b-2 rounded-lg">
+      <div>
+        <img src="" />
+        <h2>User</h2>
+      </div>
+      <div>
+        <p>description</p>
+      </div>
+      {children}
+    </div>
+  );
+}
